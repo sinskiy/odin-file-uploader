21662c7479483a725644764684696a58ab024b9e
sinskiy <sinskiy@proton.me>
2024-08-24 08:55:46 +0300

    feat: add originalName to File model in scheme

---
+++ prisma/migrations/20240824055539_add_original_file_name/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `File` table. All the data in the column will be lost.
+  - Added the required column `fileName` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `originalName` to the `File` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "name",
+ADD COLUMN     "fileName" VARCHAR(255) NOT NULL,
+ADD COLUMN     "originalName" VARCHAR(255) NOT NULL;
